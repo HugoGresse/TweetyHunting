bbabd2f06a7110e16cacfa823ccd2c9ff8cb766e
Hugo Gresse <hugo.gresse@teads.tv>
2015-02-26 13:39:50 +0100

    Folly rename res & constant to tweetyhunting

--- app/src/main/java/fr/xjet/tweetyhunting/Constant.java
+++ app/src/main/java/fr/xjet/tweetyhunting/Constant.java
@@ -7,10 +7,10 @@ public class Constant {
 
     public static String CONSUMER_KEY = "Zl48Daqmv2CdOk2XOwAUoYODK";
     public static String CONSUMER_SECRET = "tRl0O1pec5XWqBHR3hGtWDagRmjL7X1DHPQuCHJXuA14webh30";
-    public static String PREFERENCE_NAME = "fr.xjet.tweethunting.twitteroauth";
+    public static String PREFERENCE_NAME = "fr.xjet.tweetyhunting.twitteroauth";
     public static final String PREF_KEY_SECRET = "oauth_token_secret";
     public static final String PREF_KEY_TOKEN = "oauth_token";
-    public static final String CALLBACK_URL = "oauth://tweethunting";
+    public static final String CALLBACK_URL = "oauth://tweetyhunting";
     public static final String IEXTRA_AUTH_URL = "auth_url";
     public static final String IEXTRA_OAUTH_VERIFIER = "oauth_verifier";
     public static final String IEXTRA_OAUTH_TOKEN = "oauth_token";
